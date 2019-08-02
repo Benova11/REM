7f2315f23636e82790144ea3aa1fde8988d1548c
ben efrat <ben.afr@gmail.com>
2019-08-02 17:57:46 +0300

    mreged with GUI proj

---
+++ ConsoleApp1/CA.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class CA
+    {
+        const string FILENAME = "C:/Users/ben11/Desktop/remcheck/twoDarrayGrid.XML";
+
+
+
+        public void run(int[,] map)
+        {
+            int[,] grid = map;
+
+            string xml = "<Data></Data>";
+
+            XDocument doc = XDocument.Parse(xml);
+            XElement data = doc.Root;
+
+            for (int row = 0; row <= grid.GetUpperBound(0); row++)
+            {
+                XElement xRow = new XElement("Row");
+                data.Add(xRow);
+                for (int col = 0; col <= grid.GetUpperBound(1); col++)
+                {
+                    XElement xCol = new XElement("Column", grid[row, col]);
+                    xRow.Add(xCol);
+                }
+            }
+
+            XElement music = new XElement("music");
+            data.Add(music);
+            XElement soundtrack = new XElement("GEVAs_main_sountrack");
+            XElement RonWalking = new XElement("RonWalking");
+            music.Add(soundtrack);
+            music.Add(RonWalking);
+            doc.Save(FILENAME);
+
+            XDocument newDoc = XDocument.Load(FILENAME);
+            int[][] newGrid = newDoc.Descendants("Row").Select(x => x.Elements("Column").Select(y => (int)y).ToArray()).ToArray();
+            int[,] newArray = new int[newGrid.Length, newGrid[0].Length];
+
+            for (int i = 0; i < newGrid.Length; i++)
+            {
+                int[] innerArray = newGrid[i];
+
+                for (int j = 0; j < innerArray.Length; j++)
+
+                {
+                    newArray[i, j] = innerArray[j];
+                }
+            }
+
+            for (int i = 0; i < newArray.GetLength(0); i++)
+            {
+
+
+                for (int j = 0; j < newArray.GetLength(1); j++)
+
+                {
+                    Console.Write(newArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            // string CancelUrl =newDoc.GetElementsByTagName("cbt:CancelUrl")[0].InnerText;
+            //var result = newDoc.Root
+            //.Descendants("music")
+            //.FirstOrDefault()?.Value;
+            //  Console.WriteLine(result.Length);
+
+            XElement xMusic = newDoc.Descendants("music").First();
+            String s = xMusic.FirstNode.ToString();
+            Console.WriteLine(s);
+
+            xMusic.FirstNode.Remove();
+            s = xMusic.FirstNode.ToString();
+            Console.WriteLine(s);
+            Console.Read();
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+ 
--- LevelDesignerGui/Game1.cs
+++ LevelDesignerGui/Game1.cs
@@ -1,23 +1,46 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using ConsoleApp1;
 
 namespace LevelDesignerGui
 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    /// 
+    class Game1 : Game
     {
+        CA consoleApp = new CA();
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D gameBackround;
+        SpriteFont font;
+        int[,] map;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-        }
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+            //graphics.IsFullScreen = true;
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
 
+        }
+        public void DrawGrid(int[,] gameMap, SpriteBatch spriteBatch, SpriteFont f)
+        {
+            for (int x = 0; x < gameMap.GetLength(1); x++)
+            {
+                for (int y = 0; y < gameMap.GetLength(0); y++)
+                {
+                    spriteBatch.DrawString(f, y + " / " + x, new Vector2(x * 64, y * 64), Color.White);
+                }
+
+            }
+
+        }
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
@@ -37,9 +60,20 @@ namespace LevelDesignerGui
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
+            this.IsMouseVisible = true;
+            gameBackround = Content.Load<Texture2D>("level_01_A");
+            // Create a new SpriteBatch, which can be used to draw textures.
+            //   for(int i =0;i<map1.Height:i++)
+            //     for (int j=0;j<map1.Width;j++)
+
+            Console.WriteLine(gameBackround.Width / 64);
+            Console.WriteLine(gameBackround.Height / 64);
+            map = new int[gameBackround.Width / 64, gameBackround.Height / 64];
+
+            font = Content.Load<SpriteFont>("Fonts/Font");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
             // TODO: use this.Content to load your game content here
         }
 
@@ -62,10 +96,34 @@ namespace LevelDesignerGui
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            MouseState ms = Mouse.GetState();
+            if (ms.LeftButton == ButtonState.Pressed)
+            {
+                int x = Convert.ToInt32(ms.X) / 16;
+                int y = Convert.ToInt32(ms.Y) / 16 + 1;
+                //Console.WriteLine(x);
+                //Console.WriteLine(y);
+            }
 
+            var mouseState = Mouse.GetState();
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                var xIndex = mouseState.X / 64;
+                var yIndex = mouseState.Y / 64;
+
+                if (xIndex >= 0 && xIndex < map.GetLength(0) && yIndex >= 0 && yIndex < map.GetLength(1))
+                {
+                    map[yIndex, xIndex] = 1;
+                }
+            }
+            // TODO: Add your update logic here
+            if (mouseState.RightButton == ButtonState.Pressed)
+                consoleApp.run(map);
             base.Update(gameTime);
         }
+
+
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -75,9 +133,17 @@ namespace LevelDesignerGui
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            spriteBatch.Begin();
             // TODO: Add your drawing code here
 
+            spriteBatch.Draw(gameBackround, new Rectangle(0, -700, 3840, 1984), Color.White);
+            DrawGrid(map, spriteBatch, font);
+
             base.Draw(gameTime);
+            spriteBatch.End();
         }
+
+
     }
+
 }
