4513f71aa179da3ef3e8a9ced6c6f63f68999747
ben efrat <ben.afr@gmail.com>
2019-08-10 17:05:35 +0300

    healthbooster FINISHED

--- RemGame/Figures/HealthBooster.cs
+++ RemGame/Figures/HealthBooster.cs
@@ -21,6 +21,9 @@ namespace RemGame
         private Kid player;
         private Texture2D texture;
         private Rectangle collisionRec;
+        private Vector2 startingLocation;
+        private float floatingDirection;
+
 
         public HealthBooster (World world,ContentManager Content,Kid player,int value,Vector2 position,Vector2 size)
         {
@@ -30,42 +33,50 @@ namespace RemGame
             Activated = false;
             texture = Content.Load<Texture2D>("misc/HealthBooster");
             body = BodyFactory.CreateRectangle(world, size.X * CoordinateHelper.pixelToUnit, size.Y * CoordinateHelper.pixelToUnit, 1);
-            body.BodyType = BodyType.Static;
+            body.BodyType = BodyType.Dynamic;
+            body.IgnoreGravity = true;
             body.CollisionCategories = Category.Cat5;
             collisionRec = new Rectangle((int)position.X, (int)position.Y,(int)size.X,(int)size.X);
-
+            startingLocation = position;
+            floatingDirection = -1;
         }
 
         public int Value { get => value; set => this.value = value; }
         public bool Activated { get => activated; set => activated = value; }
         public Vector2 Position { get => body.Position * CoordinateHelper.unitToPixel; set => body.Position = value * CoordinateHelper.pixelToUnit; }
 
-        public void TakeOff()
-        {
-            body.BodyType = BodyType.Dynamic;
-            body.ApplyForce(new Vector2(3,-12));
-            body.Rotation += 0.2f;
-            activated = true;
-        }
-
-
         public void Update(GameTime gameTime)
         {
             if (collisionRec.Intersects(player.CollisionRec))
             {
-                TakeOff();
                 if (!activated)
                 {
                     player.Health += value;
                     player.HealthBar.increase(value);
+                    activated = true;
                 }
+            }
+
+            if (activated)
+            {
+                body.ApplyForce(new Vector2(-2, -5));
+                body.Rotation += 0.05f;
+            }
+
+            else
+            {
+                body.ApplyForce(new Vector2(0, floatingDirection));
+
+                if (Position.Y <= startingLocation.Y - 25)
+                    floatingDirection = 1;
+
+                else if (Position.Y + 25 >= startingLocation.Y)
+                    floatingDirection = -1;
             }
 
             if (this.Position.Y < -300)
             {
-
                 this.body.Enabled = false;
-
             }
         }
 
@@ -79,6 +90,7 @@ namespace RemGame
                55,
                55
            );
+
             if (body.Enabled)
             {
                 spriteBatch.Draw(texture, destination, null, Color.White, body.Rotation, new Vector2(texture.Width, texture.Height), SpriteEffects.None, 0);
